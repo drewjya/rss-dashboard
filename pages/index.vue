17432627d3debe6f7ae018f15fd6d33fcc23fb12
drewjyaa <andre@sahamrakyat.com>
2024-03-15 17:15:14 +0700

    Refactor RSS feed page and add search functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup>
 const rss = ref([]);
 
-
-
 const columns = [
   { label: "Title", key: "title" },
   { label: "Description", key: "description" },
   { label: "Type", key: "rss_type" },
   { label: "Created At", key: "created_at" },
 ];
+
+const isLoading = ref(false);
 
 const metaPage = ref({
   current_page: 1,
@@ -22,29 +22,65 @@ const metaPage = ref({
 });
 
 const page = ref(1);
+const search = ref("");
 
 watch(page, async (val) => {
+  rss.value = [];
+  isLoading.value = true;
   const load = await privateApi.get(apiPath.rssFeed, {
     baseURL: useRuntimeConfig().public.BASE_API_RSS,
     params: {
       type: "",
       page: `${val}`,
+      search: search.value,
       limit: "15",
       sort: "desc",
       by: "published_at",
     },
   });
+  isLoading.value = false;
   rss.value = load.data as any;
   metaPage.value = (load as any).meta;
 });
 
+watchDebounced(
+  search,
+  async (val) => {
+    rss.value = [];
+    const load = await privateApi.get(apiPath.rssFeed, {
+      baseURL: useRuntimeConfig().public.BASE_API_RSS,
+      params: {
+        type: "",
+        page: `${page.value}`,
+        search: val,
+        limit: "15",
+        sort: "desc",
+        by: "published_at",
+      },
+    });
+    rss.value = load.data as any;
+    isLoading.value = false;
+    metaPage.value = (load as any).meta;
+  },
+  {
+    debounce: 500,
+    onTrigger: (val) => (isLoading.value = true),
+    immediate: false,
+  }
+);
+
 const privateApi = usePrivateApi();
 const apiPath = useApiPath();
+definePageMeta({
+  colorMode: "light",
+});
 
 const isOpen = ref(false);
 
 const content = ref();
 onMounted(async () => {
+  rss.value = [];
+  isLoading.value = true;
   const val = await privateApi.get(apiPath.rssFeed, {
     baseURL: useRuntimeConfig().public.BASE_API_RSS,
     params: {
@@ -55,6 +91,7 @@ onMounted(async () => {
       by: "published_at",
     },
   });
+  isLoading.value = false;
   rss.value = val.data as any;
   metaPage.value = (val as any).meta;
 });
@@ -72,7 +109,20 @@ onMounted(async () => {
       "
     />
     <UModal v-model="isOpen"> <div v-html="content" class="p-4"></div></UModal>
-    <UTable :rows="rss" :columns="columns">
+    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+      <UInput v-model="search" placeholder="Filter people..." />
+    </div>
+    <UTable
+      :rows="rss"
+      :columns="columns"
+      :loading="isLoading"
+      :loading-state="{
+        icon: 'i-heroicons-arrow-path-20-solid',
+        label: 'Loading...',
+      }"
+      :progress="{ color: 'primary', animation: 'carousel' }"
+      class="w-full"
+    >
       <template #title-data="{ row }">
         <button
           @click="
--- pages/login.vue
+++ pages/login.vue
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 definePageMeta({
   layout: "auth",
+  colorMode: "light",
 });
 
 const { $v } = useMessage();
